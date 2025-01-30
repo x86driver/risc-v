d4b41bcebcc86064cb9b92fa0e111729a48b48b3
Shane Lin <253692+x86driver@users.noreply.github.com>
2025-01-30 21:40:49 +0800

    add x1, x0, x0 可以讀取和寫入 但因為都是 0 還沒有測試其他的數值

--- risc-v.srcs/sources_1/new/alu.sv
+++ risc-v.srcs/sources_1/new/alu.sv
@@ -4,18 +4,40 @@
 module alu_control(
     input logic [3:0] opcode,
     input logic [1:0] aluop,
-    output logic [3:0] alu_func
+    output logic [3:0] alu_ctrl
 );
 
     always_comb begin
         casez ({aluop, opcode})
-            6'b00????: alu_func = 4'b0010; // lw, sw
-            6'b?1????: alu_func = 4'b0110; // beq
-            6'b1?0000: alu_func = 4'b0010; // add
-            6'b1?1000: alu_func = 4'b0110; // sub
-            6'b1?0111: alu_func = 4'b0000; // and
-            6'b1?0110: alu_func = 4'b0001; // or
-            default:   alu_func = 4'b0000;
+            6'b00????: alu_ctrl = 4'b0010; // lw, sw
+            6'b?1????: alu_ctrl = 4'b0110; // beq
+            6'b1?0000: alu_ctrl = 4'b0010; // add
+            6'b1?1000: alu_ctrl = 4'b0110; // sub
+            6'b1?0111: alu_ctrl = 4'b0000; // and
+            6'b1?0110: alu_ctrl = 4'b0001; // or
+            default:   alu_ctrl = 4'b0000;
+        endcase
+    end
+
+endmodule
+
+module alu(
+    input logic [3:0] alu_ctrl,
+    input logic [31:0] A,
+    input logic [31:0] B,
+    output logic [31:0] alu_out,
+    output logic zero
+);
+
+    assign zero = (alu_out == 0);
+
+    always_comb begin
+        case (alu_ctrl)
+            0: alu_out = A & B;
+            1: alu_out = A | B;
+            2: alu_out = A + B;
+            6: alu_out = A - B;
+            default: alu_out = 0;
         endcase
     end
 
@@ -28,8 +50,31 @@ module instruction_memory(
 
     logic [31:0] mem [0:16];
     //assign inst = 32'h00402303; // lw t1,4(zero) # 4
-    assign inst = (pc == 0) ? 32'h00402303 : 32'h00000000;
+    assign inst = (pc == 0) ? 32'h000000b3 : 32'h00000000;
     //assign inst = mem[pc >> 2];
+
+endmodule
+
+module register_file(
+    input logic clk,
+    input logic [4:0] read_reg1,
+    input logic [4:0] read_reg2,
+    input logic [4:0] write_reg,
+    input logic RegWrite,
+    input logic [31:0] write_data,
+    output logic [31:0] read_data1,
+    output logic [31:0] read_data2
+);
+
+    logic [31:0] registers [0:31];
+    assign read_data1 = (read_reg1 == 5'b0) ? 32'h0 : registers[read_reg1];
+    assign read_data2 = (read_reg2 == 5'b0) ? 32'h0 : registers[read_reg2];
+
+    always_ff @(posedge clk) begin
+        if (RegWrite && (write_reg != 5'b0)) begin
+            registers[write_reg] <= write_data;
+        end
+    end
 
 endmodule
 
@@ -141,6 +186,14 @@ module riscv_cpu(
     logic Branch;
     logic [1:0] ALUOp;
 
+    logic [3:0] alu_ctrl;
+
+    logic [31:0] alu_out;
+    logic zero;
+
+    logic [31:0] read_data1;
+    logic [31:0] read_data2;
+
     program_counter pc_module(
         .clk(clk),
         .rst_n(rst_n),
@@ -163,6 +216,31 @@ module riscv_cpu(
         .MemWrite(MemWrite),
         .Branch(Branch),
         .ALUOp(ALUOp)
+    );
+
+    alu_control alu_control_0(
+        .opcode({inst[30], inst[14:12]}),
+        .aluop(ALUOp),
+        .alu_ctrl(alu_ctrl)
+    );
+
+    alu alu_0(
+        .alu_ctrl(alu_ctrl),
+        .A(read_data1),
+        .B(read_data2), // 應該要有 mux
+        .alu_out(alu_out),
+        .zero(zero)
+    );
+
+    register_file reg_file_0(
+        .clk(clk),
+        .read_reg1(inst[19:15]),
+        .read_reg2(inst[24:20]),
+        .write_reg(inst[11:7]),
+        .RegWrite(RegWrite),
+        .write_data(alu_out), // 應該要有 mux
+        .read_data1(read_data1),
+        .read_data2(read_data2)
     );
 
 endmodule
